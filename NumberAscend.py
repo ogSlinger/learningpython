501aad4d56d4626ee090a308334abb4416ea2ae5
ogSlinger <115181809+ogSlinger@users.noreply.github.com>
2022-11-16 03:52:21 -0600

    Optimized NumberAscend.py
    While reading a file of 10000 numbers in a file psuedo-randomized ranging from 1-100, performance improved from about 5 seconds to .11 seconds in this version.

--- NumberAscend.py
+++ NumberAscend.py
@@ -1,4 +1,4 @@
-def numberAscend (inputList, separator = ' '):
+def numberAscend (inputList, separator=' '):
 
     myInput = inputList.split(separator)
 
@@ -12,32 +12,37 @@ def numberAscend (inputList, separator = ' '):
     rank = 0
     recentNumberCount = 0
     outputList = [' '] * len(myInput)
+    index = 0
 
     #compares each element to every element in the list
     for recentNumber in myInput:
-        for element in myInput:
+        if recentNumber in myInput[:index]: #Do nothing if the number has appeared already
+            None
+        else:
+            for element in myInput:
 
-            #recentNumberCount is how many times the number appears on the list
-            #rank is the index of where the number will be written on new list
-            if recentNumber < element:
-                None
-            elif recentNumber == element:
-                recentNumberCount += 1
-            elif recentNumber > element:
+                #recentNumberCount is how many times the number appears on the list
+                #rank is the index of where the number will be written on new list
+                if recentNumber < element:
+                    None
+                elif recentNumber == element:
+                    recentNumberCount += 1
+                elif recentNumber > element:
+                    rank += 1
+
+            #Write the number down starting at rank position and going left-to-right writing for every
+            #instance of the number
+            while recentNumberCount > 0:
+                outputList[rank] = recentNumber
+                recentNumberCount -= 1
                 rank += 1
 
-        #Write the number down starting at rank position and going left-to-right writing for every
-        #instance of the number
-        while recentNumberCount > 0:
-            outputList[rank] = recentNumber
-            recentNumberCount -= 1
-            rank += 1
+            #Reset for the next number to be compared
+            rank = 0
+            recentNumberCount = 0
 
-        #Reset for the next number to be compared
-        rank = 0
-        recentNumberCount = 0
+        if index < len(myInput):
+            index += 1
 
-    #Weakness to this is the entire list gets compared to itself. So, the more there are duplicate
-    #numbers in the list, the less efficient the code becomes.
 
     return outputList[:]
